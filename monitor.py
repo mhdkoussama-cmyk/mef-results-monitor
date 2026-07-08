d4ce571b94a117acce54a99c8b94cdec3a8373df
mhdkoussama-cmyk <mhdkoussama@gmail.com>
2026-07-08 12:12:20 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,63 +1,23 @@
-import os
-import json
-import requests
-from bs4 import BeautifulSoup
-
-URLS = [
-    "https://e-recrutement.finances.gov.ma/index.aspx?ReturnUrl=%2f",
-    "https://www.emploi-public.ma/fr/concours/details/5b13aa42-f2db-4399-aa93-732e59d42ae8"
-]
-
-STATE_FILE = "state.json"
-
-TOKEN = os.environ["TELEGRAM_TOKEN"]
-CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
-
-
-def send(msg):
-    requests.post(
-        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={
-            "chat_id": CHAT_ID,
-            "text": msg
-        }
-    )
-
-
-def load_state():
-    if os.path.exists(STATE_FILE):
-        with open(STATE_FILE) as f:
-            return json.load(f)
-    return {}
-
-
-def save_state(state):
-    with open(STATE_FILE, "w") as f:
-        json.dump(state, f)
-
-
-state = load_state()
-
 for url in URLS:
 
-   try:
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
 
-    r = requests.get(
-        url,
-        headers=headers,
-        timeout=20
-    )
+        r = requests.get(
+            url,
+            headers=headers,
+            timeout=20
+        )
 
-    r.raise_for_status()
+        r.raise_for_status()
 
-except Exception as e:
-    print(f"Could not access {url}: {e}")
-    continue
+    except Exception as e:
+        print(f"Could not access {url}: {e}")
+        continue
 
-soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
 
     text = soup.get_text(" ", strip=True).lower()
 
@@ -75,5 +35,3 @@ soup = BeautifulSoup(r.text, "html.parser")
         )
 
     state[url] = trigger
-
-save_state(state)
